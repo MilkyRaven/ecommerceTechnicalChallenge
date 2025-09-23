2c94ab491355d7efaf72fe2b9bd522652afb9ba3
Milky Raven <heartonllamas@gmail.com>
2025-09-23 16:43:11 +0200

    chore: add a mock page to test shop button link

--- Q3/scripts/ui/banner.js
+++ Q3/scripts/ui/banner.js
@@ -7,7 +7,7 @@ export function createPromotionBanner(container, promotion) {
   banner.id = "promo-banner";
   banner.innerHTML = ` <h3>${promotion.promoTitle}</h3>
   <p>${promotion.promoMechanic}<p>
-  <a href="./${promotion.link}">Shop now</a>
+  <a href=".${promotion.link}.html">Shop now</a>
   <a id="more-info">More info</a>`;
 
   const moreInfo = banner.querySelector("#more-info");
